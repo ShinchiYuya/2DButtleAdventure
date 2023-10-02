1d09e16601155ea1c65b7697defaa793b8937051
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-10-02 18:37:02 +0900

    playerのゲームオーバーになる条件の追加

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] static float _timer;
+    [SerializeField] float _maxTime;
+    [SerializeField] Text _textCountDown;
+    [SerializeField] PlayerHP playerHP;
+ 
     public static GameManager instance;
-    
+
     private void Awake()
     {
         if (instance == null)
@@ -12,8 +19,32 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
         }
         else
+        {
             Destroy(this.gameObject);
+        }
+    }
+    void Start()
+    {
+        _timer = _maxTime;
     }
 
+    void Update()
+    {
+        Timer();
+    }
+
+    void Timer()
+    {
+        if (_textCountDown != null && _timer > 0f)
+        {
+            _textCountDown.text = string.Format("Time: {0:00.00}", _timer);
+            _timer -= Time.deltaTime;
+        }
+        else if (_timer <= 0)
+        {
+            playerHP.InflictDamage();
+            _textCountDown.text = "END";
+        }
+    }
 
 }
--- Assets/Script/Player/PlayerHP.cs
+++ Assets/Script/Player/PlayerHP.cs
@@ -1,18 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHP : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static PlayerHP instance;
+
+    [SerializeField] int _maxHP;
+    [SerializeField] int currentHP;
+    [SerializeField] AudioClip deathClip;
+    [SerializeField] string targetSceneName;
+ 
+    [SerializeField] float stepTime = 2f;
+
+    int damageAmount = 2147483647;
+    bool isDead = false;
+    AudioSource audioSource;
+    bool hasSceneChange = false;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isDead && !hasSceneChange)
+        {
+            //Debug.Log("1");
+            StartCoroutine(DelayedSceneChange());
+            hasSceneChange = true;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead) return; // 既に死亡している場合は処理を中断
+
+        currentHP -= damage; // ダメージを体力から減算
+
+        // 体力が0以下になった場合は敵を破壊
+        if (currentHP <= 0)
+        {
+            isDead = true; // 死亡フラグを設定
+
+            // 死亡時のサウンドを再生
+            if (deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+        }
+    }
+
+    public void InflictDamage()
+    {
+        TakeDamage(damageAmount);
+    }
+
+    IEnumerator DelayedSceneChange()
+    {
+        // 死亡時の待機秒数を指定（stepTime変数の値）
+        yield return new WaitForSeconds(stepTime);
+
+        // シーンを切り替え
+        SceneManager.LoadScene(targetSceneName);
+        //Debug.Log("2");
     }
 }
