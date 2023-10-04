af6e39bd4b193ebe6be50a6703a124775ec8d549
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-10-04 13:30:40 +0900

    enemyMoveの変更

--- Assets/Script/Enemy/EnemyMove.cs
+++ Assets/Script/Enemy/EnemyMove.cs
@@ -13,12 +13,17 @@ public class EnemyMove : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.Find(target.name);
+
+        if (target == null)
+        {
+            Debug.Log("target‚ªŒ©“–‚½‚ç‚È‚¢YO!!!");
+        }
     }
 
     void Update()
     {
         distance = Vector3.Distance(transform.position, target.transform.position);
-        Debug.Log(distance);
+        //Debug.Log(distance);
 
         if (distance < disPlayer)
         {
